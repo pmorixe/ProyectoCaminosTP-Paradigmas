35537b44789b50c6016a72c55835b1a6251d7ad8
javmo <javmo94@gmail.com>
2015-10-10 00:07:22 -0300

    HumanoVelocidad

--- src/com/allthenight/model/raza/Humano.java
+++ src/com/allthenight/model/raza/Humano.java
@@ -1,5 +1,13 @@
 package com.allthenight.model.raza;
 
-public class Humano extends Raza {
+import com.allthenight.model.camino.Zona;
 
+public class Humano extends Raza {
+	/*-----------Metodos------------*/
+	public Integer obtenerVelocidad(Zona unaZona){
+		Integer unaVelocidad;
+		unaVelocidad = unaZona.getLongitud()/unaZona.getAspereza();
+		this.setVelocidad(unaVelocidad);
+		return this.getVelocidad();
+	}
 }
